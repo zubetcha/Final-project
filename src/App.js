adf3258436642b276e00d51b4a8b17d15d702cfb
zubetcha <zuhye5@gmail.com>
2021-12-25 14:13:41 +0900

    [Conf]추가 MobileFrame.css 미디어 쿼리 추가

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { history } from './redux/ConfigureStore'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './styles/GlobalStyle'
 import theme from './styles/theme'
-import { Main, NotFound, Quiz, QuizResult, Join, Login, Mypage, PostDetail, PostEdit, PostList, PostWrite, DictEdit, DictList, DictWrite } from './pages'
+import { Main, NotFound, QuizIntro, Quiz, QuizResult, Join, Login, Mypage, PostDetail, PostEdit, PostList, PostWrite, DictEdit, DictList, DictWrite } from './pages'
 import MobileFrame from './components/MobileFrame'
 
 function App() {
@@ -19,7 +19,8 @@ function App() {
             <Route path="/join" exact component={Join} />
             <Route path="/login" exact component={Login} />
             <Route path="/mypage" exact component={Mypage} />
-            <Route path="/quiz" exact component={Quiz} />
+            <Route path="/quiz" exact component={QuizIntro} />
+            <Route path="/quiz/start" exact component={Quiz} />
             <Route path="/quiz/result" exact component={QuizResult} />
             <Route path="/post" exact component={PostList} />
             <Route path="/post/detail/:postId" exact component={PostDetail} />
@@ -27,7 +28,7 @@ function App() {
             <Route path="/post/edit/:postId" exact component={PostEdit} />
             <Route path="/dict" exact component={DictList} />
             <Route path="/dict/write" exact component={DictWrite} />
-            <Route path="/dict/edit/:wordId" exact component={DictEdit} />
+            <Route path="/dict/edit/:dictId" exact component={DictEdit} />
             {/* <Route path="" exact component={NotFound} /> */}
           </ConnectedRouter>
         </MobileFrame>
