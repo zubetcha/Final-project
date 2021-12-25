c2b697c1ffb002b9af4cf505d6b70434b84f10e7
undriedspring <hansam5615@gmail.com>
2021-12-26 03:46:19 +0900

    [Design]로그인/회원가입/마이페이지/메인페이지

--- src/pages/user/Mypage.js
+++ src/pages/user/Mypage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../../styles/css/Mypage.css'
-import EditIcon from '@mui/icons-material/Edit'
 
 const Mypage = (props) => {
   return (
@@ -12,19 +11,40 @@ const Mypage = (props) => {
             <text className="UserProfileName">Username</text>
             <div className="VerticalLine" />
             <text className="UserProfileAge">20대</text>
-            <EditIcon />
           </div>
           <div className="UserActivityInfo">
+            <div className="UserMyDictTag">
+              <text className="UserMyDict">단어</text>
+              <text className="UserMyDictCount">5개</text>
+            </div>
             <div className="UserMyPostTag">
-              <text className="UserMyPost">내가 쓴 게시물</text>
+              <text className="UserMyPost">게시물</text>
               <text className="UserMyPostCount">10개</text>
             </div>
             <div className="UserGetMyLikeTag">
-              <text className="UserGetMyLike">내가 받은 좋아요</text>
+              <text className="UserGetMyLike">좋아요</text>
               <text className="USerGetMyLikeCount">3개</text>
             </div>
           </div>
-          <button className="UserProfileUpdateButton">프로필 수정</button>
+        </div>
+        <div className="UserMyPostList">
+          <div className="UserMyPostListButton1">전체</div>
+          <div className="UserMyPostListButton2">최신순</div>
+        </div>
+        <div className="UserMyPostListCard">
+          <div>어쩔티비 저쩔티비</div>
+        </div>
+        <div className="UserMyPostListCard">
+          <div>어쩔티비 저쩔티비</div>
+        </div>
+        <div className="UserMyPostListCard">
+          <div>어쩔티비 저쩔티비</div>
+        </div>
+        <div className="UserMyPostListCard">
+          <div>어쩔티비 저쩔티비</div>
+        </div>
+        <div className="UserMyPostListCard">
+          <div>어쩔티비 저쩔티비</div>
         </div>
       </div>
     </>
