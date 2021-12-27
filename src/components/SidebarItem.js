a2e9efdd076a320e33e62bad9cd971e18d3982e8
zubetcha <zuhye5@gmail.com>
2021-12-27 22:53:14 +0900

    [Design]완료 헤더 사이드바 뷰 1차 작업 및 페이지 이동 연결 완료

--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -6,7 +6,7 @@ import { history } from '../redux/ConfigureStore'
 const SidebarItem = ({ menu, isActive, setShowSidebar }) => {
   return (
     <>
-      <button
+      <MenuItem
         className={`${isActive ? 'active' : ''}`}
         onClick={() => {
           history.push(`${menu.path}`)
@@ -14,9 +14,18 @@ const SidebarItem = ({ menu, isActive, setShowSidebar }) => {
         }}
       >
         {menu.name}
-      </button>
+      </MenuItem>
     </>
   )
 }
+
+const MenuItem = styled.button`
+  width: 100%;
+  padding: 20px 16px;
+  border-bottom: 1px solid #111;
+  text-align: left;
+  font-weight: 700;
+  font-size: 16px;
+`
 
 export default SidebarItem
