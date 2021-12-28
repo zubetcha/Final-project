c4b88a9fd7ac8a56e8cff432267ee71eb6d41deb
undriedspring <hansam5615@gmail.com>
2021-12-28 18:48:15 +0900

    [Feat]용어사전 작성 및 페이지네이션

--- src/pages/dict/DictList.js
+++ src/pages/dict/DictList.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import '../../styles/css/DictList.css'
 import { useDispatch, useSelector } from 'react-redux'
+import { history } from '../../redux/ConfigureStore'
 import axios from 'axios'
-import OneDictionaryCard from '../../components/OneDictionaryCard'
+// import OneDictionaryCard from '../../components/OneDictionaryCard'
 import { actionCreators as dictActions } from '../../redux/modules/dict'
 import Pagination from 'rc-pagination'
 
 const DictList = (props) => {
   const dispatch = useDispatch()
 
+  // const dictId = Number(props.match.params.dictId)
+
   const [dict, setDict] = useState([])
   const [size, setSize] = useState(10)
   const [page, setPage] = useState(1)
-  const [totalCount, setTotalCount] = useState(100)
+  const [totalCount, setTotalCount] = useState()
 
   const handlePageChange = (page) => {
     setDict({ ...setDict, Page: page })
@@ -34,7 +37,13 @@ const DictList = (props) => {
   return (
     <>
       <div className="DictLayout">
+        <div className="NewDictAddButtonSection">
+          <div className="NewDictAddButton" onClick={() => history.push('/dict/write')}>
+            + 새로운 용어 등록하기
+          </div>
+        </div>
         <div className="DictList">
+<<<<<<< HEAD
           {dict.map((d) => {
             return (
               <div className="OneDictionaryCard" key={d.id}>
@@ -43,6 +52,14 @@ const DictList = (props) => {
               </div>
             )
           })}
+=======
+          {dict.map((dict) => (
+            <div className="OneDictionaryCardList" key={dict.id}>
+              <div className="OneDictionaryCardList WordCardList1">{dict.title}</div>
+              <div className="OneDictionaryCardList WordCardList2"></div>
+            </div>
+          ))}
+>>>>>>> 06e6973 ([Feat]용어사전 작성 및 페이지네이션)
         </div>
         <Pagination simple page={page} total={totalCount} size={size} onChange={(page) => setPage(page)} />
       </div>
