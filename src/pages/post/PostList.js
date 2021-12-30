bd3767dd860671582b114aa370bdf09d8a78bcc4
zhiyeonyi <yjy39k@naver.com>
2021-12-30 12:38:32 +0900

    [Feat]post수정중

--- src/pages/post/PostList.js
+++ src/pages/post/PostList.js
@@ -11,6 +11,8 @@ const PostList = (props) => {
   const history = useHistory()
   const dispatch = useDispatch()
 
+
+
   const postList = useSelector((state) => state.post.list) // state는 리덕스 스토어의 전체 데이터
   console.log(postList)
 
