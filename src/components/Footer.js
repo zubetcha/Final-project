541a304c5705b250f39b9484bbeeebf90aa336a3
zubetcha <zuhye5@gmail.com>
2022-01-14 23:55:21 +0900

    [Design] 하단 내비바 뷰 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
 import Grid from '../elements/Grid'
-import SmileIcon from '../styles/image/smileIcon_Yellow.png'
+import SmileIcon from '../styles/image/smileIcon_Blue.png'
 import { RiBookMarkLine, RiGamepadLine } from 'react-icons/ri'
 import { MdOutlinePhotoLibrary } from 'react-icons/md'
 import { CgProfile } from 'react-icons/cg'
@@ -14,62 +14,71 @@ const Footer = (props) => {
   }
   return (
     <>
-      <NavFooter>
-        <ul className="nav-list">
-          <li className="nav-item">
-            <NavLink to="/dict" className="nav-link" activeStyle={activeStyle}>
-              <Grid flex_center column>
-                <RiBookMarkLine className="nav-icon" />
-                <div className="nav-link__text">사전</div>
-              </Grid>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/image" className="nav-link" activeStyle={activeStyle}>
-              <Grid flex_center column>
-                <MdOutlinePhotoLibrary className="nav-icon" />
-                <div className="nav-link__text">짤방</div>
-              </Grid>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/" className="nav-link fixed">
-              <Grid flex_center column>
-                <Logo src={SmileIcon} />
-              </Grid>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/quiz" className="nav-link" activeStyle={activeStyle}>
-              <Grid flex_center column>
-                <RiGamepadLine className="nav-icon" />
-                <div className="nav-link__text">퀴즈</div>
-              </Grid>
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/mypage" className="nav-link" activeStyle={activeStyle}>
-              <Grid flex_center column>
-                <CgProfile className="nav-icon" />
-                <div className="nav-link__text">MY</div>
-              </Grid>
-            </NavLink>
-          </li>
-        </ul>
-      </NavFooter>
+      <NavContainer>
+        <NavBar>
+          <ul className="nav-list">
+            <li className="nav-item">
+              <NavLink to="/dict" className="nav-link" activeStyle={activeStyle}>
+                <Grid flex_center column>
+                  <RiBookMarkLine className="nav-icon" />
+                  <div className="nav-link__text">사전</div>
+                </Grid>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/image" className="nav-link" activeStyle={activeStyle}>
+                <Grid flex_center column>
+                  <MdOutlinePhotoLibrary className="nav-icon" />
+                  <div className="nav-link__text">짤방</div>
+                </Grid>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link fixed">
+                <Grid flex_center column>
+                  <Logo src={SmileIcon} />
+                </Grid>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/quiz" className="nav-link" activeStyle={activeStyle}>
+                <Grid flex_center column>
+                  <RiGamepadLine className="nav-icon" />
+                  <div className="nav-link__text">퀴즈</div>
+                </Grid>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/mypage" className="nav-link" activeStyle={activeStyle}>
+                <Grid flex_center column>
+                  <CgProfile className="nav-icon" />
+                  <div className="nav-link__text">MY</div>
+                </Grid>
+              </NavLink>
+            </li>
+          </ul>
+        </NavBar>
+      </NavContainer>
     </>
   )
 }
 
-const NavFooter = styled.nav`
+const NavContainer = styled.div`
+  width: 100%;
+  padding: 0 16px 10px;
   position: absolute;
-  bottom: 0;
   left: 0;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+`
+
+const NavBar = styled.nav`
   width: 100%;
   height: 74px;
-  background-color: #f0f0f0;
-  border-top-left-radius: 20px;
-  border-top-right-radius: 20px;
+  background-color: ${({ theme }) => theme.colors.yellow};
+  border-radius: 20px;
+  border: 2px solid #111;
   padding: 12px 10px 10px;
   z-index: 1000;
 
@@ -109,18 +118,17 @@ const NavFooter = styled.nav`
         position: fixed;
         left: 50%;
         transform: translateX(-50%);
-        bottom: 24px;
+        bottom: 40px;
       }
     }
   }
 `
 
 const Logo = styled.div`
-  width: 60px;
-  height: 60px;
+  width: 52px;
+  height: 52px;
   border: 2px solid #111;
   cursor: pointer;
-  background-color: #fff;
   background-size: cover;
   background-image: url('${(props) => props.src}');
   background-position: center;
