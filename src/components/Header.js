977dcf4b618af323a41a528e236accc978fe2f7f
zubetcha <zuhye5@gmail.com>
2022-01-16 21:03:58 +0900

    [Design] Q&A 디테일 페이지 뷰 수정중

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,8 +14,7 @@ const Header = ({ type, children, location }) => {
   const dispatch = useDispatch()
   const profile = useSelector((state) => state.mypage.myProfile)
   const userId = localStorage.getItem('id')
-  const token = document.cookie.split('; ').find((row) => row.startsWith('token'))
-  // .split('=')[1]
+  const token = document.cookie.split('=')[1]
   const isLogin = userId !== null && token !== undefined ? true : false
 
   const documentRef = useRef(document)
