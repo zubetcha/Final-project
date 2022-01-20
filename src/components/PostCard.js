815dee902fa2738c010fd96c970758fe2b9db33a
zhiyeonyi <yjy39k@naver.com>
2022-01-20 09:59:29 +0900

    [Design]질문 나도궁금해요 아이콘 변경

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -7,6 +7,9 @@ import { ReactComponent as ViewIcon } from '../styles/icons/조회_18dp.svg'
 import { ReactComponent as EmptyHeartIcon } from '../styles/icons/하트 비활성_24dp.svg'
 import { ReactComponent as FullHeartIcon } from '../styles/icons/하트 활성_24dp.svg'
 import { ReactComponent as CommentIcon } from '../styles/icons/댓글_18dp.svg'
+import { ReactComponent as ICuriousToo} from '../styles/icons/quiz_black_24dp.svg'
+
+
 import { BiBadge, BiBadgeCheck } from 'react-icons/bi'
 import Grid from '../elements/Grid'
 
@@ -56,40 +59,26 @@ const PostCard = ({ question }) => {
         <Wrap questionList={question} onClick={onC}>
           <Grid flex_align>
             <CuriousQ>Q</CuriousQ>
-            {/* <UserImg src={question.profileImageUrl} alt="" /> */}
             <div className="profile-box">
             <Title>{question && question.title}</Title>
-
-              {/* <Writer>{question.writer}</Writer> */}
-              {/* <div className="created-date">
-                <CreatedAt>{question.createdAt.split('T')[0]}</CreatedAt>
-                <CreatedAt>{hour.split(':')[0] + ':' + hour.split(':')[1]}</CreatedAt>
-              </div> */}
             </div>
           </Grid>
           <Content>{question&&question.content}</Content>
-          {/* <Title>{question && question.title}</Title> */}
           <Icon>
             <IconBox>
               <ViewIcon fill="#333" />
               <Number>{question && question.views}</Number>
             </IconBox>
             <IconBox>
-              {isCuriousToo ? <FullHeartIcon fill="#333" onClick={handleClickCuriousToo} /> : <EmptyHeartIcon fill="#333" onClick={handleClickCuriousToo} />}
+              {isCuriousToo ? <ICuriousToo fill="#00A0FF" onClick={handleClickCuriousToo} /> : <ICuriousToo fill="#333" onClick={handleClickCuriousToo} />}
               <Number className="like-count">{curiousTooCnt}</Number>
             </IconBox>
             <IconBox>
               <CommentIcon fill="#333" />
               <Number>{question && question.commentCnt}</Number>
             </IconBox>
-            {/* <IconBox>{question.isComplete ? <BiBadgeCheck size="20" /> : <BiBadge size="20" />}</IconBox> */}
           </Icon>
         </Wrap>
-        {/* {question.thumbNail ? (
-          <ThumbNailContainer>
-            <ThumbNail className="uploadimg" src={question && question.thumbNail} alt="" />
-          </ThumbNailContainer>
-        ) : null} */}
       </FullWrap>
     </>
   )
@@ -138,28 +127,6 @@ const CuriousQ = styled.div`
 
 `
 
-const UserImg = styled.div`
-  margin: 0 10px 0 0;
-  width: 36px;
-  height: 36px;
-  border-radius: 20px;
-  background-size: cover;
-  background-image: url('${(props) => props.src}');
-  background-position: center;
-`
-const Writer = styled.p`
-  width: 100%;
-  font-family: 'YdestreetL';
-  font-style: normal;
-  font-weight: normal;
-  font-size: ${({ theme }) => theme.fontSizes.base};
-`
-
-const CreatedAt = styled.div`
-  font-size: ${({ theme }) => theme.fontSizes.small};
-  margin: 0 5px 0 0;
-`
-
 const Title = styled.div`
   font-size: ${({ theme }) => theme.fontSizes.lg};
   font-style: normal;
@@ -192,15 +159,6 @@ const Number = styled.p`
   margin: 0 9.5px 0 5px;
 `
 
-const ThumbNailContainer = styled.div`
-  display: flex;
-  align-items: flex-end;
-`
-
-const ThumbNail = styled.img`
-  width: 80px;
-  height: 80px;
-`
 const IconBox = styled.div`
   display: flex;
   align-items: center;
